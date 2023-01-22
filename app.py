4824763dc13271480eadffa60dc157acb2241c2e
vrm2000 <vrm2000@uma.es>
2023-01-22 16:13:55 +0100

    Added image upload support

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 from flask import Flask, render_template, session, abort, redirect, request, url_for
-import database, os, pathlib, requests, json
+import database
+import os
+import pathlib
+import requests
+import json
 from dotenv import load_dotenv
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
 from bson.objectid import ObjectId
+from werkzeug.utils import secure_filename
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = "your-secret-key"
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+UPLOAD_FOLDER = '\\static\\img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_credentials.json")
+client_secrets_file = os.path.join(pathlib.Path(
+    __file__).parent, "client_credentials.json")
 
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
-    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    scopes=["https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email", "openid"],
     redirect_uri="http://127.0.0.1:8500/callback"
 )
 
 
 client = os.getenv("DB_URI")
+
 
 def login_is_required(function):
     def wrapper(*args, **kwargs):
@@ -34,9 +44,11 @@ def login_is_required(function):
 
     return wrapper
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/home')
 @login_is_required
@@ -48,10 +60,10 @@ def home():
     }
     game_list_db = database.db.games.find()
     for i in game_list_db:
-        if(i['winner_name'] == ''):
-                i['winner_name'] = 'Pendiente'
+        if (i['winner_name'] == ''):
+            i['winner_name'] = 'Pendiente'
 
-        if(i['user_uuid'] == session['google_id']):
+        if (i['user_uuid'] == session['google_id']):
             game_list['my_games'].append(i)
         else:
             game_list['games'].append(i)
@@ -68,13 +80,15 @@ def login():
     session["state"] = state
     return redirect(authorization_url)
 
+
 @app.route("/callback")
 def callback():
     flow.fetch_token(authorization_response=request.url)
     credentials = flow.credentials
     request_session = requests.session()
     cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+    token_request = google.auth.transport.requests.Request(
+        session=cached_session)
     id_info = id_token.verify_oauth2_token(
         id_token=credentials._id_token,
         request=token_request,
@@ -83,20 +97,24 @@ def callback():
     session['google_id'] = id_info.get("sub")
     session['name'] = id_info.get("name")
     user_exist = database.db.users.find_one({"uuid": session['google_id']})
-    if(not user_exist):
-        database.db.users.insert_one({"uuid": session['google_id'], "name": session['name']})
+    if (not user_exist):
+        database.db.users.insert_one(
+            {"uuid": session['google_id'], "name": session['name']})
     print(session['google_id'])
     return redirect("/home")
+
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
 
+
 @app.route("/protected_area")
 def protected_area():
     print(session)
     return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+
 
 @app.route("/test")
 def test():
@@ -104,28 +122,41 @@ def test():
     database.db.collection.insert_one({"name": "John"})
     return "Connected to the data base!"
 
+
 def logout():
     session.clear()
+
 
 @app.route("/create")
 def create():
     print(session)
     return render_template("create.html")
 
+
 @app.route("/create", methods=['POST'])
 def save_game():
     obj = json.loads('[{}]'.format(request.form['caches']))
     for i in range(0, len(obj)):
         obj[i]['clue'] = request.form['clue' + str(i)]
+        obj[i]['image'] = request.files['image' + str(i)].filename
+        if obj[i]['image'] != '':
+            f = request.files['image' + str(i)]
+            f.save(f.filename)
+            os.rename(os.path.abspath(f.filename), os.path.abspath(".")+'/static/img/' +
+                      request.form['game_name']+'_'+request.form['clue' + str(i)]+'_'+f.filename)
 
-    database.db.games.insert_one({"user_uuid": session['google_id'], "user_name": session['name'], "name": request.form['game_name'], "north": request.form['north'], "south": request.form['south'], "east": request.form['east'], "west": request.form['west'], "caches": obj, "number_caches": len(obj), "winner_name": "", "active": True})
+    print(obj)
+    database.db.games.insert_one({"user_uuid": session['google_id'], "user_name": session['name'], "name": request.form['game_name'], "north": request.form['north'],
+                                 "south": request.form['south'], "east": request.form['east'], "west": request.form['west'], "caches": obj, "number_caches": len(obj), "winner_name": "", "active": True})
     return render_template("create.html")
+
 
 @app.route("/play")
 def play():
     game_id = request.args.get('game')
     game_data_db = database.db.games.find_one({"_id": ObjectId(game_id)})
     return render_template("play.html", game_data=game_data_db)
+
 
 @app.route("/game")
 def game():
@@ -134,10 +165,11 @@ def game():
 
     return render_template("game.html", game_data=game_data_db)
 
+
 @app.route("/game", methods=["POST"])
 def restart():
-    if(request.method == "POST"):
+    if (request.method == "POST"):
         print("helooo")
         # game_data_db = database.db.games.update_one({"_id": ObjectId(game_id)})
-    
+
     return 'helo'
--- static/create.js
+++ static/create.js
@@ -1,19 +1,19 @@
 const map = L.map('map').setView([40, -3.7], 8);
 var newMarker, count = 0, cache_data = [], cache_string = "";
 
-$(function(){
+$(function () {
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-            maxZoom: 18
-        }).addTo(map);
-        newMarkerGroup = new L.LayerGroup();
-        map.on('click', addMarker);
-        function addMarker(e){
-            newMarker = new L.marker(e.latlng).addTo(map);
-        }
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxZoom: 18
+    }).addTo(map);
+    newMarkerGroup = new L.LayerGroup();
+    map.on('click', addMarker);
+    function addMarker(e) {
+        newMarker = new L.marker(e.latlng).addTo(map);
+    }
 });
 
-$('#addRectangle').click(function(event) {
+$('#addRectangle').click(function (event) {
     $('#dataRectangle').show();
     $('#cacheRow').show();
     var north = map.getBounds().getSouth();
@@ -21,15 +21,14 @@ $('#addRectangle').click(function(event) {
     var east = map.getBounds().getEast();
     var west = map.getBounds().getWest();
     bounds = [[south, west], [north, east]]
-    L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(map);
+    L.rectangle(bounds, { color: "#ff7800", weight: 1 }).addTo(map);
     map.fitBounds(bounds);
     $('input[name="north"]').val(north);
     $('input[name="south"]').val(south);
     $('input[name="east"]').val(east);
     $('input[name="west"]').val(west);
 });
-
-$('#map').click(function(event) {
+$('#map').click(function (event) {
     var coordinates = newMarker.getLatLng();
     $('#cacheRow').append(
         `<div class="row my-2">
@@ -40,17 +39,36 @@ $('#map').click(function(event) {
                 <input type="text" class="form-control w-100" value="${coordinates.lng}" readonly>
             </div>
             <div class="col-4">
-                <input type="text" class="form-control w-100" name="clue${count}" placeholder="Pista" required>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#clue${count}">
+                    Crear Pista
+                </button>
+                
+                <!-- Modal -->
+                <div class="modal fade" id="clue${count}" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Pista ${count}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                        <div class="modal-body">
+                            <input type="text" class="form-control w-100" name="clue${count}" placeholder="Pista" required>
+                            <br>
+                            <input type="file" name="image${count}" accept="image/png, image/gif, image/jpeg, image/jpg" />
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>`
     );
     cache_string += count == 0 ? `{"latitude":${coordinates.lat}, "longitude": ${coordinates.lng}}` : `, {"latitude":${coordinates.lat}, "longitude": ${coordinates.lng}}`;
-    console.log(cache_string);
+    console.log($('#cacheRow'));
     $('input[name="caches"]').val(cache_string);
-    cache_data.push({latitude: coordinates.lat, longitude: coordinates.lng});
+    cache_data.push({ latitude: coordinates.lat, longitude: coordinates.lng });
     count++;
 });
 
-$('#addGameData').click(function(event) {
-    
+$('#addGameData').click(function (event) {
+
 });
