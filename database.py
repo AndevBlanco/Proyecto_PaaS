af0497172839789f738056f5505588dcb83f49b7
Fabio Blanco <fblanco@deepspacerobots.com>
2023-01-22 17:01:56 +0100

    Files deleted

--- database.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-
-CONNECTION_STRING = "mongodb+srv://AndevBlanco:QFdMRnuNRsw3AkvF@geocaching.ipjyzwj.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('geo')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
