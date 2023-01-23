1203c62c430fa4dad12e6f06f1e6058e145d6e29
Fabio Blanco <fblanco@deepspacerobots.com>
2023-01-23 08:21:40 +0100

    Set point on game supervision template

--- static/game.js
+++ static/game.js
@@ -11,21 +11,17 @@ var north = $('#dataRectangleNorth').val();
 var south = $('#dataRectangleSouth').val();
 var east = $('#dataRectangleEast').val();
 var west = $('#dataRectangleWest').val();
+var data_caches = $('#dataCaches').val();
 var bounds = [[south, west], [north, east]];
-console.log(bounds);
 map.fitBounds(bounds);
 
 var transform_obj = data_caches.replace(/'/g, '"');
 var transform_obj2 = transform_obj.replace(/False/g, false);
 var transform_obj3 = transform_obj2.replace(/True/g, true);
-console.log(transform_obj3);
 var obj = JSON.parse(transform_obj3);
 for(var i in obj){
     if(obj[i]['found']){
-        console.log(obj[i]);
-
         let coords = L.latLng(obj[i]['latitude'], obj[i]['longitude']);
-        console.log("Coood. ", coords);
         new L.marker(coords).addTo(map);
     }
 }
